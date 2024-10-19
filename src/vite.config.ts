ad1cb7e422b68ae12e1e8f9ab5ba3edfa351b37e
Mark Fawcett <markjfawcett@gmail.com>
2024-10-19 21:43:44 +0100

    Vite React starter working

---
+++ src/vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // Set the root directory to 'reactUi' insted of default 'src'
+  // root: 'reactUi',
+  // consider changing the build dir
+  // build: {
+  //   outDir: '../dist', // Adjust the output directory if needed
+  // },
+});
